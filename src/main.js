1feb6fa5bf5f43938b963ff9b1246dd6b5f9aae1
BobZielony <alan.lachur@gmail.com>
2025-06-09 13:38:19 +0200

    zadanie 12

--- src/main.js
+++ src/main.js
@@ -1,24 +1,57 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import {format} from 'date-fns'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1 class="text-2xl font-bold">Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button" class="bg-blue-300 p-4"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const articles = document.getElementById("articles");
+const form = document.getElementById("form");
 
-setupCounter(document.querySelector('#counter'))
+const fetchArticles = async () => {
+  try {
+    const response = await fetch(
+      'https://buuksufdwjpcnnrppnjn.supabase.co/rest/v1/article?select=*', {
+      headers: {
+        apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1dWtzdWZkd2pwY25ucnBwbmpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4ODY1NjksImV4cCI6MjA2NDQ2MjU2OX0.jEbJAINd59eRkfsCLRJ56M1NioszFOraP988iiYnv1s',
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+    let articleString = "";
+    data.forEach(element => {
+      articleString += `<article>
+      <p>Tytuł: ${element.title}</p>
+      <p>Podtytuł: ${element.subtitle}</p>
+      <p>Autor: ${element.author}</p>
+      <p>Data utworzenia: ${format(new Date(element.created_at),"dd-MM-yyyy")}</p>
+      <p>Treść: ${element.content}</p></article>`
+    });
+    articles.innerHTML = articleString;
+  } catch (error) {
+    console.error('Fetch error:', error);
+  }
+};
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  await createNewArticle(form);
+})
+
+const createNewArticle = async (article) => {
+  try {
+    const response = await fetch('https://buuksufdwjpcnnrppnjn.supabase.co/rest/v1/article', {
+      method: 'POST',
+      headers: {
+        apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1dWtzdWZkd2pwY25ucnBwbmpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4ODY1NjksImV4cCI6MjA2NDQ2MjU2OX0.jEbJAINd59eRkfsCLRJ56M1NioszFOraP988iiYnv1s',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ author:article["author"].value,title:article["title"].value,subtitle:article["subtitle"].value,content:article["content"].value }),
+    });
+
+    if (response.status !== 201) {
+      throw new Error(`Status: ${response.status}`);
+    }
+  } catch (error) {
+    console.error('Fetch error:', error);
+  }
+};
+
+
+fetchArticles();
